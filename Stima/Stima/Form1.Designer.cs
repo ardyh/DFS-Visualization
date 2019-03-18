07c55daa788c011d2ccd5333820b5e0c5c470a5a
irhw110 <41326866+irhw110@users.noreply.github.com>
2019-03-19 01:29:57 +0700

    Utama

---
+++ Stima/Stima/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Stima
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Msagl.Core.Geometry.Curves.PlaneTransformation planeTransformation4 = new Microsoft.Msagl.Core.Geometry.Curves.PlaneTransformation();
+            this.buttonsolve = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.gViewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Answers = new System.Windows.Forms.ListBox();
+            this.Queries = new System.Windows.Forms.ListBox();
+            this.Clean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonsolve
+            // 
+            this.buttonsolve.Location = new System.Drawing.Point(26, 367);
+            this.buttonsolve.Name = "buttonsolve";
+            this.buttonsolve.Size = new System.Drawing.Size(131, 44);
+            this.buttonsolve.TabIndex = 0;
+            this.buttonsolve.Text = "Solve";
+            this.buttonsolve.UseVisualStyleBackColor = true;
+            this.buttonsolve.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // gViewer
+            // 
+            this.gViewer.ArrowheadLength = 10D;
+            this.gViewer.AsyncLayout = false;
+            this.gViewer.AutoScroll = true;
+            this.gViewer.BackwardEnabled = false;
+            this.gViewer.BuildHitTree = true;
+            this.gViewer.CurrentLayoutMethod = Microsoft.Msagl.GraphViewerGdi.LayoutMethod.UseSettingsOfTheGraph;
+            this.gViewer.EdgeInsertButtonVisible = true;
+            this.gViewer.FileName = "";
+            this.gViewer.ForwardEnabled = false;
+            this.gViewer.Graph = null;
+            this.gViewer.InsertingEdge = false;
+            this.gViewer.LayoutAlgorithmSettingsButtonVisible = true;
+            this.gViewer.LayoutEditingEnabled = true;
+            this.gViewer.Location = new System.Drawing.Point(311, 12);
+            this.gViewer.LooseOffsetForRouting = 0.25D;
+            this.gViewer.MouseHitDistance = 0.05D;
+            this.gViewer.Name = "gViewer";
+            this.gViewer.NavigationVisible = true;
+            this.gViewer.NeedToCalculateLayout = true;
+            this.gViewer.OffsetForRelaxingInRouting = 0.6D;
+            this.gViewer.PaddingForEdgeRouting = 8D;
+            this.gViewer.PanButtonPressed = false;
+            this.gViewer.SaveAsImageEnabled = true;
+            this.gViewer.SaveAsMsaglEnabled = true;
+            this.gViewer.SaveButtonVisible = true;
+            this.gViewer.SaveGraphButtonVisible = true;
+            this.gViewer.SaveInVectorFormatEnabled = true;
+            this.gViewer.Size = new System.Drawing.Size(477, 426);
+            this.gViewer.TabIndex = 3;
+            this.gViewer.TightOffsetForRouting = 0.125D;
+            this.gViewer.ToolBarIsVisible = true;
+            this.gViewer.Transform = planeTransformation4;
+            this.gViewer.UndoRedoButtonsVisible = true;
+            this.gViewer.WindowZoomButtonPressed = false;
+            this.gViewer.ZoomF = 1D;
+            this.gViewer.ZoomWindowThreshold = 0.05D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Solve External";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.solveEksternal);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 326);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 22);
+            this.textBox1.TabIndex = 14;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(182, 367);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(103, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(221, 326);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(64, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 326);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Answers
+            // 
+            this.Answers.FormattingEnabled = true;
+            this.Answers.ItemHeight = 16;
+            this.Answers.Location = new System.Drawing.Point(182, 103);
+            this.Answers.Name = "Answers";
+            this.Answers.Size = new System.Drawing.Size(103, 196);
+            this.Answers.TabIndex = 15;
+            // 
+            // Queries
+            // 
+            this.Queries.FormattingEnabled = true;
+            this.Queries.ItemHeight = 16;
+            this.Queries.Location = new System.Drawing.Point(26, 103);
+            this.Queries.Name = "Queries";
+            this.Queries.Size = new System.Drawing.Size(131, 196);
+            this.Queries.TabIndex = 16;
+            this.Queries.SelectedIndexChanged += new System.EventHandler(this.Queries_SelectedIndexChanged);
+            // 
+            // Clean
+            // 
+            this.Clean.Location = new System.Drawing.Point(174, 29);
+            this.Clean.Name = "Clean";
+            this.Clean.Size = new System.Drawing.Size(111, 44);
+            this.Clean.TabIndex = 18;
+            this.Clean.Text = "Clean";
+            this.Clean.UseVisualStyleBackColor = true;
+            this.Clean.Click += new System.EventHandler(this.Clean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clean);
+            this.Controls.Add(this.Queries);
+            this.Controls.Add(this.Answers);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gViewer);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonsolve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonsolve;
+        private System.Windows.Forms.Button button2;
+        private Microsoft.Msagl.GraphViewerGdi.GViewer gViewer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox Answers;
+        private System.Windows.Forms.ListBox Queries;
+        private System.Windows.Forms.Button Clean;
+    }
+}
+
---
+++ Stima/Stima/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Edge = Microsoft.Msagl.Drawing.Edge;
+using Node = Microsoft.Msagl.Drawing.Node;
+using Point = Microsoft.Msagl.Core.Geometry.Point;
+using Rectangle = Microsoft.Msagl.Core.Geometry.Rectangle;
+using GeomEdge = Microsoft.Msagl.Core.Layout.Edge;
+
+namespace Stima
+{
+    public partial class Form1 : Form
+    {
+        int[,] array;
+        int nEdges;
+        int n;
+        int[] antrian;
+        int caraJalan, neffJalur, neffAntrian;
+        int tmptJose, tmptFerdiand;
+        int[] jalur;
+        int[,] data;
+        int[] dikunjungi;
+        bool jalanAja;
+        Microsoft.Msagl.Drawing.Graph graph;
+
+        public Form1()
+        {
+            int i, j;
+            ReadGraph();
+            InitializeComponent();
+            //create a graph object 
+           graph = new Microsoft.Msagl.Drawing.Graph("graph");
+
+            for (j = 0; j < nEdges; j++)
+            {
+                graph.AddEdge(Convert.ToString(array[j, 0]), Convert.ToString(array[j, 1])).Attr.ArrowheadAtTarget = Microsoft.Msagl.Drawing.ArrowStyle.None;
+            }
+            //bind the graph to the viewer 
+            gViewer.Graph = graph;
+
+            
+            
+            neffAntrian = 0;
+            neffJalur = 0;
+            antrian = new int[n + 1];
+            jalur = new int[n + 1];
+            dikunjungi = new int[n];
+            data = new int[n, n];
+            for (i = 0; i < n + 1; i++)
+            {
+                antrian[i] = 0;
+                jalur[i] = 0;
+            }
+            for (i = 0; i < n; i++)
+            {
+                dikunjungi[i] = 0;
+            }
+
+            for(int a=0; a<nEdges; a++)
+            {
+                i = array[a,0];
+                j = array[a,1];
+                data[i - 1, j - 1] = 1;
+                data[j - 1, i - 1] = 1;
+            }
+
+
+            /*
+            //create the graph content 
+            graph.AddEdge("A", "B");
+            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+            */
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void buttonsolve_Click()
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int x = Convert.ToInt32(textBox2.Text)-1;
+            int y = Convert.ToInt32(textBox3.Text)-1;
+
+            
+            //Nampilin jawaban
+            if (Solusi(a, x, y))
+            {
+                textBox4.Text = "Ya";
+            }
+            else
+            {
+                textBox4.Text = "Tidak";
+            }
+        }
+        
+
+        private void solveEksternal(object sender, EventArgs e)
+        {
+            TextReader tr = new StreamReader("query.txt");
+            int a, x, y;
+            int nquery = Convert.ToInt32(tr.ReadLine());
+
+            string s;
+            while ((s = tr.ReadLine()) != null)
+            {
+                string[] strtemp = s.Split(' ');
+                a = Convert.ToInt32(strtemp[0]);
+                x = Convert.ToInt32(strtemp[1]);
+                y = Convert.ToInt32(strtemp[2]);
+
+                //insert dfs
+                if(Solusi(a, x-1, y-1))
+                {
+                    Answers.Items.Add("YA");
+                }
+                else
+                {
+                    Answers.Items.Add("TIDAK");
+                }
+
+                // Nampilin query
+                Queries.Items.Add(s);
+
+            }
+            tr.Close();
+        }
+        
+
+        public void resetVariabel() {
+            for(int i=0; i<n+1; i++)
+            {
+                jalur[i] = 0;
+            }
+            neffJalur = 0;
+            for (int i = 0; i < n; i++) {
+                dikunjungi[i] = 0;
+            }
+        }
+
+        public void ReadGraph()
+        {
+            TextReader tr = new StreamReader("input.txt");
+            int i = 0;
+
+            n = Convert.ToInt32(tr.ReadLine());
+            nEdges = n - 1;
+            array = new int[nEdges, 2];
+
+            string s;
+            while ((s = tr.ReadLine()) != null)
+            {
+                string[] strtemp = s.Split(' ');
+                array[i, 0] = Convert.ToInt32(strtemp[0]);
+                array[i, 1] = Convert.ToInt32(strtemp[1]);
+                i++;
+            }
+            tr.Close();
+        }
+
+        public Boolean Solusi(int a,int b,int c)
+        {
+            caraJalan = a;
+            tmptJose = b;
+            tmptFerdiand = c;
+            jalanAja = false;
+            DFS(0);
+            resetVariabel();
+            return jalanAja;
+        }
+
+        public int GetSimpul(int i, int j)
+        {
+            return data[i, j];
+        }
+
+        public int xSudahKunjungi(int x)
+        {
+            return dikunjungi[x];
+        }
+
+        public void SetDikunjungi(int x)
+        {
+            dikunjungi[x] = 1;
+        }
+
+        public void SetJalur(int value)
+        {
+            jalur[neffJalur] = value;
+            neffJalur++;
+        }
+
+        public void setAntrian(int value)
+        {
+            antrian[neffAntrian] = value;
+            neffAntrian++;
+        }
+
+        public void ResetJalur(int value)
+        {
+            neffJalur = value;
+        }
+
+        public bool xIsDaun(int x)
+        {
+            bool isDaun = true;
+            for (int i = 0; i < n; i++)
+            {
+                if (GetSimpul(x, i) == 1 && dikunjungi[i] == 0)
+                {
+                    isDaun = false;
+                    break;
+                }
+            }
+
+            return isDaun;
+        }
+
+        public void DFS(int x)
+        {
+            if (jalanAja == false)
+            {
+                SetDikunjungi(x);
+                SetJalur(x + 1);
+                setTetangganX(x);
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            buttonsolve_Click();
+        }
+
+        private void Clean_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                graph.FindNode(Convert.ToString(i + 1)).Attr.FillColor = Microsoft.Msagl.Drawing.Color.White;
+            }
+        }
+
+        private void Queries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String[] query = (Convert.ToString(Queries.SelectedItem)).Split(' ') ;
+            int a = Convert.ToInt32(query[0]);
+            int x = Convert.ToInt32(query[1]);
+            int y = Convert.ToInt32(query[2]);
+
+            Boolean tem =Solusi(a, x - 1, y - 1) ;
+        }
+
+        public void cekJalur()
+        {
+            int place1 = -99;
+            int place2 = -99;
+            for (int i = 0; i < neffJalur; i++)
+            {
+                if (jalur[i] == (tmptJose + 1))
+                {
+                    place1 = i;
+                }
+                if (jalur[i] == (tmptFerdiand + 1))
+                {
+                    place2 = i;
+                }
+            }
+            if (place1 != -99 && place2 != -99)
+            {
+                if (caraJalan == 0)
+                {
+                    if (place1 < place2)
+                    {
+                        jalanAja = true;
+                        for (int i = 0; i < neffJalur; i++)
+                        {
+                            graph.FindNode(Convert.ToString(jalur[i])).Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+                        }
+                    }
+                }
+                else
+                {
+                    if (place2 < place1)
+                    {
+                        jalanAja = true;
+                        for (int i = 0; i < neffJalur ; i++)
+                        {
+                            graph.FindNode(Convert.ToString(jalur[i])).Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PrintJalur()
+        {
+            for (int i = 0; i < neffJalur; i++)
+            {
+                Console.Write(jalur[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        public void setTetangganX(int x)
+        {
+            int v = neffJalur;
+            bool isDaun = xIsDaun(x);
+            if (caraJalan == 0)
+            {
+                if (isDaun || tmptFerdiand == x)
+                {
+                    cekJalur();
+                }
+                else
+                {
+                    if (jalanAja == false)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            if (GetSimpul(x, i) == 1 && xSudahKunjungi(i) == 0)
+                            {
+                                DFS(i);
+                                ResetJalur(v);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (isDaun || tmptJose == x)
+                {
+                    cekJalur();
+                }
+                else
+                {
+                    if (jalanAja == false)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            if (GetSimpul(x, i) == 1 && xSudahKunjungi(i) == 0)
+                            {
+                                DFS(i);
+                                ResetJalur(v);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        }
+}
